fe34eca050467cc5cb76856c9fdf7eb4a48c69b1
meza68k <meza24k@gmail.com>
2022-06-16 05:26:58 -0500

    finsished shop

--- assets/journey-tourney.js
+++ assets/journey-tourney.js
@@ -1,10 +1,14 @@
+//intro
+var playerName = window.prompt("Welcome to the Journey Tourney. What will your name be?");
+ gameIntro();
+ window.confirm("Hello "+playerName+" are you ready to fight?"); 
 // var playerName = window.prompt("What is your players name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerGold = 20;
 
 //logging multiple values like this 
-console.log(playerName, playerAttack, playerHealth);
+//console.log(playerName, playerAttack, playerHealth);
 //array of enemies
 var enemyNames = ["bender", "will smith","mrSteelyotoe", "joemama."];
 var enemyHealth = 50;
@@ -28,12 +32,12 @@ function displayPlayer() {
 };
 
 //displays enemys stats
-function displayOpp() {
+function displayOpp(i) {
     window.alert(enemyNames[i]+ " currently has "+ enemyHealth+" HP, and " + enemyAttack + "AP.");
     console.log(enemyNames[i]+ " currently has "+ enemyHealth+" HP, and " + enemyAttack + "AP.");
 };
 //gives description of the round
-function displayRnd() {
+function displayRnd(i) {
     console.log(
     playerName + " attacked " + enemyNames[i] + ". " + enemyNames[i] + " now has "+ enemyHealth+ " hp remaining." );
     window.alert( playerName + " attacked " + enemyNames[i] + ". " + enemyNames[i] + " now has "+ enemyHealth+ " hp remaining." );
@@ -41,13 +45,12 @@ function displayRnd() {
     console.log( enemyNames[i] + " attacked " + playerName + ". " + playerName + " now has "+ playerHealth+ " hp remaining." );
 };
 //introudction
-function introudction(){
-    var playerName = window.prompt("What is your players name?");
-    window.alert("Welcome to the Journey Tourney! " + playerName);
-    gameIntro();
-    window.confirm("Are you ready to fight?"); 
-};
-introudction();
+// function introudction(){
+//     var playerName = window.prompt("Welcome to the Journey Tourney. What will your name be?");
+//     gameIntro();
+//     window.confirm("Hello "+playerName+" are you ready to fight?"); 
+// };
+
 //Displays enemy now has
 function nowOppHas(){
     window.alert(enemyNames[i]+" now has "+enemyHealth+" HP");
@@ -63,25 +66,55 @@ function nowGold() {
 };
 //used to restart the for loop without messing up the rounds because of an error
 var cancelI = 0
+//items
+var refillHp = function() {
+    playerHealth = 100
+    playerGold = playerGold - 10;
 
+};
+var upgradeAp = function() {
+    playerAttack = playerAttack + 5;
+    playerGold = playerGold - 5;
+
+};
+//the shop
+function shop(roundNum) {
+    var shopSel = window.prompt("Congratulations on making it to round "+(roundNum)+". Would you like to visit the shop Now?");
+    if (shopSel === "yes" || shopSel === "YES") {
+        window.alert("Welcome to the shop, please enter health , upgrade , or enter exit to exit the shop at anymoment.")
+        var itemSel = window.prompt("health costs 10gs, and upgrading your AP costs 5gs. ")
+        switch(itemSel){
+            case "health":refillHp() ;break;
+            case "upgrade":upgradeAp(); break;
+            case "exit": 
+            };
+    }else if (shopSel === "no" || shopSel === "NO"){
+    }else {};
+
+ };
 //
-function fight(enemyName) {
+function fight(enemyName, i) {
     enemyHealth = 50;
-    displayPlayer();
-    displayOpp();
-    var playerSel = window.prompt("welcome to round "+ (i+1)+"! Would you like to take on "+ enemyName+"? or skip for 5 gold.");
+    debugger;
+    var roundNum = (i+1)
+    if (i >= 1){
+        shop(roundNum);
+    };
+    displayPlayer(i);
+    displayOpp(i);
+    var playerSel = window.prompt("welcome to round "+roundNum+"! Would you like to take on "+ enemyName+"? or skip for 5 gold.");
     if (playerSel === "fight" || playerSel === "FIGHT") {
         while(enemyHealth > 0 && playerHealth > 0 ){
         playerHealth = playerHealth-enemyAttack;
         enemyHealth = enemyHealth-playerAttack;
-        displayRnd();
+        displayRnd(i);
         };
         if (playerHealth <= 0){
             window.alert("You just got taken out by "+enemyName+". refresh to retry");
             exit;
         };
         if (enemyHealth <= 0) {
-            window.alert(enemyAttack+" has been taken out of the Tourney.");
+            window.alert(enemyName+" has been taken out of the Tourney.");
             playerGold = playerGold + 5;
             
         };
@@ -105,12 +138,30 @@ function fight(enemyName) {
     };
 };
 
-for(var i = 0 ;i < enemyNames.length; i = i+1+cancelI) {
-     //debugger;
-    fight(enemyNames[i]);
+function startGame(){
+    for(var i = 0 ;i < enemyNames.length; i = i+1+cancelI) {
+        //debugger;
+       fight(enemyNames[i],i);
+   };
 };
-
-window.alert("congratulations on Winning the journey Tourney "+ playerName+"!");
+// for(var i = 0 ;i < enemyNames.length; i = i+1+cancelI) {
+//      //debugger;
+//     fight(enemyNames[i]);
+// };
+startGame()
+function endGame() {
+    window.alert("congratulations on Winning the journey Tourney "+ playerName+"!");
+    var tryAg = window.confirm("would you like to try again?");
+    if (tryAg) {
+        gameIntro();
+        startGame();
+    }
+    else {
+        window.alert("Have a good day")
+        exit;
+    };
+};
+endGame();
 
 // for(var i = 0;i < enemyNames.length; i++) {
 //     fight(enemyNames[i]);
