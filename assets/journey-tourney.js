e3fbf0875bd68c6999703dd41d349d75adfd2288
meza68k <meza24k@gmail.com>
2022-06-16 03:14:11 -0500

    working game

--- assets/journey-tourney.js
+++ assets/journey-tourney.js
@@ -1,19 +1,20 @@
-var playerName = window.prompt("What is your players name?");
+// var playerName = window.prompt("What is your players name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerGold = 20;
 
 //logging multiple values like this 
 console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roberto";
+//array of enemies
+var enemyNames = ["bender", "will smith","mrSteelyotoe", "joemama."];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 function penalty(){
 
 };
-var skippingPen = -1;
+//the cost to skip
+var skippingPen = 5;
 
 //explaining the game a bit
 function gameIntro() {
@@ -25,86 +26,92 @@ function displayPlayer() {
     window.alert(playerName+ " currently has "+ playerHealth+" HP, " + playerAttack + " AP, and "+playerGold+" Gold");
     console.log(playerName+ " currently has "+ playerHealth+" HP "+ playerAttack + " AP, and "+playerGold+" Gold");
 };
+
 //displays enemys stats
 function displayOpp() {
-    window.alert(enemyName+ " currently has "+ enemyHealth+" HP, and " + enemyAttack + "AP.");
-    console.log(enemyName+ " currently has "+ enemyHealth+" HP, and " + enemyAttack + "AP.");
+    window.alert(enemyNames[i]+ " currently has "+ enemyHealth+" HP, and " + enemyAttack + "AP.");
+    console.log(enemyNames[i]+ " currently has "+ enemyHealth+" HP, and " + enemyAttack + "AP.");
 };
 //gives description of the round
 function displayRnd() {
     console.log(
-    playerName + " attacked " + enemyName + ". " + enemyName + " now has "+ enemyHealth+ " hp remaining." );
-    window.alert( playerName + " attacked " + enemyName + ". " + enemyName + " now has "+ enemyHealth+ " hp remaining." );
-    window.alert( enemyName + " attacked " + playerName + ". " + playerName + " now has "+ playerHealth+ " hp remaining." );
+    playerName + " attacked " + enemyNames[i] + ". " + enemyNames[i] + " now has "+ enemyHealth+ " hp remaining." );
+    window.alert( playerName + " attacked " + enemyNames[i] + ". " + enemyNames[i] + " now has "+ enemyHealth+ " hp remaining." );
+    window.alert( enemyNames[i] + " attacked " + playerName + ". " + playerName + " now has "+ playerHealth+ " hp remaining." );
+    console.log( enemyNames[i] + " attacked " + playerName + ". " + playerName + " now has "+ playerHealth+ " hp remaining." );
 };
 //introudction
 function introudction(){
+    var playerName = window.prompt("What is your players name?");
     window.alert("Welcome to the Journey Tourney! " + playerName);
     gameIntro();
     window.confirm("Are you ready to fight?"); 
 };
 introudction();
-//player selection of fight
-function promptInitFight() {
-   // newOpp();
-    displayPlayer();
-    window.alert("Your opponent will be "+enemyName+", a middle aged blender with rusty blades. With a total of "+enemyHealth+" HP, and "+enemyAttack+" AP.")
-    playerSelect = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose."); 
-    if (playerSelect === "FIGHT" || "fight") {
-        window.alert("you have chosen to "+ playerSelect)
-        enemyHealth = (enemyHealth-playerAttack);
-        playerHealth = (playerHealth-enemyAttack);
-        displayRnd();
-        if (playerHealth <= 0 ) { 
-            Window.alert(playerName+" Got his ass whooped by "+ enemyName)
-        } 
-        else if (enemyHealth <= 0 ) {
-            window.alert(enemyName+" has been busted!")
-            playergold = (playerGold + 5)
-        } 
-        else (enemyHealth && playerHealth >= 0);
-            fightAgain();
-    
-        
-    }else if (playerSelect === "SKIP" || "skip") {
-        window.alert("you have chosen to "+ playerSelect)
-        var confirmSkip = window.prompt("Are You sure You want to skip?");
-        if (confirmSkip) {
-           windows.alert(playerName+" Has lost 5 gold for being scared.");
-           playerGold = (playerGold - skippingPen);
-       // newOpp();
-        }
-        else  {
-            promptInitFight();
-        }
+//Displays enemy now has
+function nowOppHas(){
+    window.alert(enemyNames[i]+" now has "+enemyHealth+" HP");
+};
+//displays playernow
+function playerNowhas(){
+    window.alert(playerNames[i]+" now has "+ enemyHealth+" HP");
+};
+//dislpays player gold
+function nowGold() {
+    window.alert(playerName+" Now has "+playerGold+" gold.");
+    console.log(playerName+" Now has "+playerGold+" gold.");
+};
+//used to restart the for loop without messing up the rounds because of an error
+var cancelI = 0
 
+//
+function fight(enemyName) {
+    enemyHealth = 50;
+    displayPlayer();
+    displayOpp();
+    var playerSel = window.prompt("welcome to round "+ (i+1)+"! Would you like to take on "+ enemyName+"? or skip for 5 gold.");
+    if (playerSel === "fight" || playerSel === "FIGHT") {
+        while(enemyHealth > 0 && playerHealth > 0 ){
+        playerHealth = playerHealth-enemyAttack;
+        enemyHealth = enemyHealth-playerAttack;
+        displayRnd();
+        };
+        if (playerHealth <= 0){
+            window.alert("You just got taken out by "+enemyName+". refresh to retry");
+            exit;
+        };
+        if (enemyHealth <= 0) {
+            window.alert(enemyAttack+" has been taken out of the Tourney.");
+            playerGold = playerGold + 5;
+            
+        };
+
+    } else if (playerSel === "skip" || playerSel === "SKIP") {
+        var skipConfirm = window.confirm("Are You sure you want to skip 5 gold will be deducted");
+        if (skipConfirm = true && playerGold >= 5){
+        playerGold = playerGold-skippingPen;
+        nowGold(); 
+        debugger;
+        };
+        if (skipConfirm = true && playerGold < 5){
+            windows.alert("Yeah your gold card declined dawg, you finna have throw them hands on this one.");
+        };
+    } else if (playerSel === "EXIT" || playerSel === "exit"){
+        exit;
     }else {
-        window.alert("Not asking again, fight or flight?")
-    }
+        window.alert("not asking again. Fight or flight?");
+        cancelI = cancelI - 1;
+        debugger;
+    };
 };
 
-function fightAgain() {
-    window.prompt("Would you like to FIGHT again or Skip? Enter 'FIGHT' or 'SKIP' to choose.");
-    playerSelect = window.prompt;
-    if (playerSelect === "FIGHT" || "fight"){
-        enemyHealth = (enemyHealth-playerAttack);
-        playerHealth = (playerHealth-enemyAttack);
-        if (playerHealth <= 0 ) { 
-            Window.alert(playerName+" Got his ass whooped by "+ enemyName)
-        } else if (enemyHealth <= 0 ) {
-            window.alert(enemyName+" has been busted!")
-        } else  {
-            displayRnd();
-            fightAgain();
-        };
-        
-    }else if (playerSelect === "SKIP" || "skip") {
-        window.alert("you have chosen to "+ playerSelect)
-        windows.alert(playerName+" Has lost 5 gold for being scared.");
-        playerGold = (playerGold - skippingPen);
-    
-        }else {
-        window.alert("Not asking again, fight or flight?")
-    }
-}
-promptInitFight(); 
+for(var i = 0 ;i < enemyNames.length; i = i+1+cancelI) {
+     //debugger;
+    fight(enemyNames[i]);
+};
+
+window.alert("congratulations on Winning the journey Tourney "+ playerName+"!");
+
+// for(var i = 0;i < enemyNames.length; i++) {
+//     fight(enemyNames[i]);
+// };
